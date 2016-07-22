b7bcfe9f0bf3dd2bdfc6f56aad160999e0e2e4ff
aditya.s <aditya.s@ucs.consulting>
2016-07-22 11:10:47 +0530

    Added the Login Api

---
+++ SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/dao/ILoginDAO.java
@@ -0,0 +1,7 @@
+package com.ucs.rsa.dao;
+
+public interface ILoginDAO {
+	
+	String numberExist(long mobileNumber, String gcmId);
+
+}
---
+++ SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/service/ILoginService.java
@@ -0,0 +1,7 @@
+package com.ucs.rsa.service;
+
+public interface ILoginService {
+	
+	String numberExist(long mobileNumber, String gcmId);
+
+}
