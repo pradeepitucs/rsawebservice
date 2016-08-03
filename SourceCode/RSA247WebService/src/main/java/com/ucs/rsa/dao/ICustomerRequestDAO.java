e597409fa7f70e435f7248646fb6ca8cba8503db
pradeepitucs <vinod.b@pradeepit.com>
2016-08-03 12:02:00 +0530

    not needed

--- SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/dao/ICustomerRequestDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ucs.rsa.dao;
-
-import com.ucs.rsa.common.dto.CustomerRequestDTO;
-
-public interface ICustomerRequestDAO {
-	
-	public CustomerRequestDTO updateUser(CustomerRequestDTO iCustomerRequestDTO, boolean isCreate);
-
-}
