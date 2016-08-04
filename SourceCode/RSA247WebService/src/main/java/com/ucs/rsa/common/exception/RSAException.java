e469a37b6a503bb1eb6164d8c6656ec91bdbc2fd
Vinod <Vinod@DESKTOP-D3FODFV>
2016-08-04 11:40:20 +0530

    added

---
+++ SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/common/exception/RSAException.java
@@ -0,0 +1,62 @@
+package com.ucs.rsa.common.exception;
+
+import java.util.Locale;
+
+import com.ucs.rsa.common.constants.RSAErrorConstants.ErrorCode;
+
+public class RSAException extends RuntimeException {
+
+	private static final long serialVersionUID = -1238436298527570673L;
+
+	private Locale locale;
+	private String localizedErrorMessage;
+
+	private Throwable rootCause;
+	private ErrorCode error;
+
+	public RSAException() {
+
+	}
+
+	public RSAException(ErrorCode iErrorCode) {
+		this.error = iErrorCode;
+	}
+
+	public RSAException(ErrorCode iErrorCode, Locale iLocale) {
+		this.error = iErrorCode;
+		this.locale = iLocale;
+	}
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+
+	public String getLocalizedErrorMessage() {
+		return localizedErrorMessage;
+	}
+
+	public void setLocalizedErrorMessage(String localizedErrorMessage) {
+		this.localizedErrorMessage = localizedErrorMessage;
+	}
+
+	public ErrorCode getError() {
+		return error;
+	}
+
+	public void setError(ErrorCode error) {
+		this.error = error;
+	}
+
+	public Throwable getRootCause() {
+		return rootCause;
+	}
+
+	public void setRootCause(Throwable rootCause) {
+		this.rootCause = rootCause;
+	}
+
+}
