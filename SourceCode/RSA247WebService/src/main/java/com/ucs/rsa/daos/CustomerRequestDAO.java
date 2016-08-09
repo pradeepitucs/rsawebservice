79964108dae9e715d4efc817a3eea9c729cd8cfa
aditya.s <aditya.s@ucs.consulting>
2016-08-09 15:45:44 +0530

    Handle Customer Request.

--- SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/daos/CustomerRequestDAO.java
+++ SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/daos/CustomerRequestDAO.java
@@ -1,9 +1,17 @@
 package com.ucs.rsa.daos;
 
+import java.util.ArrayList;
+
 import com.ucs.rsa.model.CustomerRequestModel;
 
 public interface CustomerRequestDAO extends BaseDAO{
 	
 	public CustomerRequestModel updateCustomerRequest(CustomerRequestModel iCustomerRequestModel);
+	
+    public ArrayList<Integer> getServiceProviderIDS(ArrayList<Double> ratingAndLocation, String serviceType);
+	
+	public ArrayList<String> getDeviceIDSFromServiceProviderIDS(ArrayList<Integer> serviceProviderIDs);
+	
+	public String getIssueStatus(int issueID);
 
 }
