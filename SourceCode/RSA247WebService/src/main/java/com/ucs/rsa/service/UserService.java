cd1bdff10d9d47e477fb5a746f74401ad00db8ff
aditya.s <aditya.s@ucs.consulting>
2016-08-09 15:42:46 +0530

    Added the Employee Insert Related code

--- SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/service/UserService.java
+++ SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ucs.rsa.service;
 
 import com.ucs.rsa.model.CustomerModel;
+import com.ucs.rsa.model.EmployeeModel;
 import com.ucs.rsa.model.ServiceProviderModel;
 
 public interface UserService extends BaseService {
@@ -8,6 +9,8 @@ public interface UserService extends BaseService {
 	public CustomerModel updateCustomer(CustomerModel iCustomerModel);
 	
 	public ServiceProviderModel updateServiceProvider(ServiceProviderModel iCustomerModel);
+	
+	public EmployeeModel updateEmployee(EmployeeModel iCustomerModel);
 	
 	public String login(String iMobileNo, String iGcmId);
 
