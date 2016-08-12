11cf829af086fa81fc32385b9d1d51a8f7cfafea
aditya.s <aditya.s@ucs.consulting>
2016-08-12 17:37:25 +0530

    Added the methods for getting required data for service proivder and customer

--- SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/service/CustomerRequestService.java
+++ SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/service/CustomerRequestService.java
@@ -3,15 +3,26 @@ package com.ucs.rsa.service;
 import java.util.ArrayList;
 
 import com.ucs.rsa.model.CustomerRequestModel;
+import com.ucs.rsa.model.EmployeeModel;
 
 public interface CustomerRequestService extends BaseService {
 
 	public CustomerRequestModel updateCustomerRequest(CustomerRequestModel iCustomerRequestModel);
+	
+	public ArrayList<String> updateCustomerRequestByEmployee(CustomerRequestModel iCustomerRequestModel);
 	
 	public ArrayList<Integer> getServiceProviderIDS(ArrayList<Double> ratingAndLocation, String serviceType);
 	
 	public ArrayList<String> getDeviceIDSFromServiceProviderIDS(ArrayList<Integer> serviceProviderIDs);
 	
 	public String getIssueStatus(int issueID);
+	
+	public String getGCMIDFromCustomerID(int customerId);
+	
+	public int getServiceProviderIDFromEmployeeID(int employeeID);
+	
+	public ArrayList<Double> getServiceProviderLocationFromServiceProviderId(int serviceProviderID);
+	
+	public EmployeeModel getEmployeeDataFromEmployeeID(int employeeID);
 
 }
