62d7e14efe6d6420d7354d1b09b9f39c5428e9f3
aditya.s <aditya.s@ucs.consulting>
2016-09-07 17:59:32 +0530

    Added more API for Service Provider and Customer's Registration and Login.

--- SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/service/UserService.java
+++ SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/service/UserService.java
@@ -1,59 +1,42 @@
-/*
- * Copy rights @ 2016, Uniqueware Consulting Pvt Ltd
- */
 package com.ucs.rsa.service;
+
+import java.util.ArrayList;
 
 import com.ucs.rsa.model.CustomerModel;
 import com.ucs.rsa.model.EmployeeModel;
 import com.ucs.rsa.model.ServiceProviderModel;
+import com.ucs.rsa.model.ServiceProviderServiceMatchingModel;
+import com.ucs.rsa.model.UserVehicleModel;
 
+public interface UserService extends BaseService {
 
-/**
- * The Interface UserService.
- *
- * @author Gururaj A M
- * @version 1.0
- * 
- */
-public interface UserService extends BaseService
-{
-
-	/**
-	 * Update customer.
-	 *
-	 * @param iCustomerModel
-	 *           the i customer model
-	 * @return the customer model
-	 */
 	public CustomerModel updateCustomer(CustomerModel iCustomerModel);
 
-	/**
-	 * Update service provider.
-	 *
-	 * @param iCustomerModel
-	 *           the i customer model
-	 * @return the service provider model
-	 */
 	public ServiceProviderModel updateServiceProvider(ServiceProviderModel iCustomerModel);
 
-	/**
-	 * Update employee.
-	 *
-	 * @param iCustomerModel
-	 *           the i customer model
-	 * @return the employee model
-	 */
 	public EmployeeModel updateEmployee(EmployeeModel iCustomerModel);
 
-	/**
-	 * Login.
-	 *
-	 * @param iMobileNo
-	 *           the i mobile no
-	 * @param iGcmId
-	 *           the i gcm id
-	 * @return the string
-	 */
-	public String login(String iMobileNo, String iGcmId);
+	public String login(long iMobileNo, String iGcmId);
+	
+	public String employeeLogin(long iMobileNo, String iGcmId);
+
+	public ServiceProviderServiceMatchingModel getServiceProvidermatchingModel(
+			ArrayList<ServiceProviderServiceMatchingModel> serviceProviderServiceMatchingModelList);
+	
+	public EmployeeModel insertEmployeesData(EmployeeModel employees);
+	
+	public String approveServiceProvider(int serviceProviderId);
+	
+	public String approveEmployee(int employeeId);
+	
+	public String acceptTermAndCondition(String imageFolderName,boolean accepttermAndCondition);
+	
+	public CustomerModel updateMemberShip(int userId, boolean isMembership);
+	
+	public UserVehicleModel getUservehicleDetail(int userId);
+	
+	public EmployeeModel getEmployeeIdFromNumber(long employeeNumber);
+	
+	public ServiceProviderModel getServiceProviderIdFromNumber(long serviceProviderNumber);
 
 }
