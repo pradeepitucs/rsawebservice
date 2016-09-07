6655f6c2f02be2a4fb2fcf379605f3d698f0ad9c
aditya.s <aditya.s@ucs.consulting>
2016-09-07 17:47:41 +0530

    Employee Dashboard.

---
+++ SourceCode/RSA247WebService/src/main/java/com/ucs/rsa/service/impl/DefaultEmployeeDashboardService.java
@@ -0,0 +1,57 @@
+package com.ucs.rsa.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.ucs.rsa.daos.EmployeeDashboardRequestDAO;
+import com.ucs.rsa.model.EmployeeModel;
+import com.ucs.rsa.service.EmployeeDashboardService;
+
+@Service
+public class DefaultEmployeeDashboardService extends DefaultBaseService implements EmployeeDashboardService{
+
+	@Autowired
+	@Qualifier("defaultEmployeeDashboardDAO")
+	EmployeeDashboardRequestDAO employeeDashboardRequestDAO;
+	
+	@Override
+	public int getCompletedOrRejectedCount(String status, int employeeId) {
+		int count =0;
+		if(status!=null && employeeId!=0) {
+			count = employeeDashboardRequestDAO.getCompletedOrRejectedCount(status, employeeId);
+		}
+		return count;
+	}
+
+	@Override
+	public double getEmployeeRating(int employeeId) {
+		double rating =0;
+		if( employeeId!=0) {
+			rating = employeeDashboardRequestDAO.getEmployeeRating(employeeId);
+		}
+		return rating;
+	}
+
+	@Override
+	public List<EmployeeModel> getEmployeeFromServiceProviderId(int serviceProviderId) {
+		List<EmployeeModel> employees =null;
+		if( serviceProviderId!=0) {
+			employees = employeeDashboardRequestDAO.getEmployeeFromServiceProviderId(serviceProviderId);
+		}
+		return employees;
+	}
+
+	@Override
+	public int getCompletedOrRejectedCountForServiceProvider(String status, List<EmployeeModel> employees) {
+		int count =0;
+		if(status!=null && employees!=null) {
+			count = employeeDashboardRequestDAO.getCompletedOrRejectedCountForServiceProvider(status, employees);
+		}
+		return count;
+	}
+
+}
