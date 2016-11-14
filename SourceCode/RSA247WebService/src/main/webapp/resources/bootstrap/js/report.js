67003e91b19e839f0bed7c98130ce0106ea5f334
Gururaj AM <Gururaj@Gururaj AM-PC>
2016-11-14 11:23:53 +0530

    reports with start date and end date, added bootstrap datetimepicker.

--- SourceCode/RSA247WebService/src/main/webapp/resources/bootstrap/js/report.js
+++ SourceCode/RSA247WebService/src/main/webapp/resources/bootstrap/js/report.js
@@ -4,27 +4,10 @@ function generateReport(reportName) {
 
 	var reporturl = "/RSA247WebService/newReports?ReportName=" + reportName
 			+ "&ReportFormat=html";
-	
+
 	$("#reportData")
-	.html("Loading...<br><img src='/RSA247WebService/resources/bootstrap/images/loading_icon.gif' align='middle' >");
-
-	$('#reportData').load(reporturl, function(response, status, xhr) {
-		if (status == "error") {
-			var msg = "Sorry but there was an error getting details ! ";
-			$("#reportData").html(msg + xhr.status + " " + xhr.statusText);
-		}
-	});
-
-	currentReportName = reportName;
-}
-
-function generateReport1(reportName, Status) {
-
-	var reporturl = "/RSA247WebService/newReports?ReportName=" + reportName
-			+ "&Status=" + Status + "&ReportFormat=html";
-	
-	$("#reportData")
-	.html("Loading...<br><img src='/RSA247WebService/resources/bootstrap/images/loading_icon.gif' align='middle' >");
+			.html(
+					"Loading...<br><img src='/RSA247WebService/resources/bootstrap/images/loading_icon.gif' align='middle' >");
 
 	$('#reportData').load(reporturl, function(response, status, xhr) {
 		if (status == "error") {
@@ -42,10 +25,144 @@ function downloadReport(format) {
 		alert("Please Select the report.");
 		return;
 	}
-	
+
 	var reporturl = "/RSA247WebService/newReports?ReportName="
 			+ currentReportName + "&ReportFormat=" + format;
 
 	window.location.href = reporturl;
+
+}
+
+function ServiceProvidersReport(reportName, format) {
+
+	var startDate = $('#startDate').find("input").val().replace(/ /gi, "%20");
+	var endDate = $('#endDate').find("input").val().replace(/ /gi, "%20");
+
+	if (startDate == "") {
+		alert("Please Select the Start Date.");
+		return;
+	}
+	if (endDate == "") {
+		alert("Please Select the End Date.");
+		return;
+	}
+	var reporturl = "";
+
+	if (format === "html") {
+		reporturl = "/RSA247WebService/newReports?ReportName=" + reportName
+				+ "&StartDate=" + startDate + "&EndDate=" + endDate
+				+ "&ReportFormat=html";
+		$("#reportData")
+				.html(
+						"Loading...<br><img src='/RSA247WebService/resources/bootstrap/images/loading_icon.gif' align='middle' >");
+
+		$('#reportData').load(reporturl, function(response, status, xhr) {
+			if (status == "error") {
+				var msg = "Sorry but there was an error getting details ! ";
+				$("#reportData").html(msg + xhr.status + " " + xhr.statusText);
+			}
+		});
+	} else {
+		reporturl = "/RSA247WebService/newReports?ReportName=" + reportName
+				+ "&StartDate=" + startDate + "&EndDate=" + endDate
+				+ "&ReportFormat=" + format;
+		window.location.href = reporturl;
+	}
+	currentReportName = reportName;
+}
+
+function CustomersReport(reportName, format) {
+
+	var startDate = $('#startDate').find("input").val().replace(/ /gi, "%20");
+	var endDate = $('#endDate').find("input").val().replace(/ /gi, "%20");
+
+	if (startDate == "") {
+		alert("Please Select the Start Date.");
+		return;
+	}
+	if (endDate == "") {
+		alert("Please Select the End Date.");
+		return;
+	}
+
+	var reporturl = "";
+
+	if (format === "html") {
+		reporturl = "/RSA247WebService/newReports?ReportName=" + reportName
+				+ "&StartDate=" + startDate + "&EndDate=" + endDate
+				+ "&ReportFormat=html";
+		$("#reportData")
+				.html(
+						"Loading...<br><img src='/RSA247WebService/resources/bootstrap/images/loading_icon.gif' align='middle' >");
+
+		$('#reportData').load(reporturl, function(response, status, xhr) {
+			if (status == "error") {
+				var msg = "Sorry but there was an error getting details ! ";
+				$("#reportData").html(msg + xhr.status + " " + xhr.statusText);
+			}
+		});
+	} else {
+		reporturl = "/RSA247WebService/newReports?ReportName=" + reportName
+				+ "&StartDate=" + startDate + "&EndDate=" + endDate
+				+ "&ReportFormat=" + format;
+		window.location.href = reporturl;
+	}
+	currentReportName = reportName;
+}
+
+function CustomerIssueReport(reportName, format) {
+
+	var startDate = $('#startDate').find("input").val().replace(/ /gi, "%20");
+	var endDate = $('#endDate').find("input").val().replace(/ /gi, "%20");
+	var statusVal = prompt("Enter the status : ");
+
+	if (startDate == "") {
+		alert("Please Select the Start Date.");
+		return;
+	}
+	if (endDate == "") {
+		alert("Please Select the End Date.");
+		return;
+	}
+	var reporturl = "";
+	if (format == "html") {
+		reporturl = "/RSA247WebService/newReports?ReportName=" + reportName
+				+ "&Status=" + statusVal + "&StartDate=" + startDate
+				+ "&EndDate=" + endDate + "&ReportFormat=html";
+
+		$("#reportData")
+				.html(
+						"Loading...<br><img src='/RSA247WebService/resources/bootstrap/images/loading_icon.gif' align='middle' >");
+		$('#reportData').load(reporturl, function(response, status, xhr) {
+			if (status == "error") {
+				var msg = "Sorry but there was an error getting details ! ";
+				$("#reportData").html(msg + xhr.status + " " + xhr.statusText);
+			}
+		});
+
+	} else {
+		reporturl = "/RSA247WebService/newReports?ReportName=" + reportName
+				+ "&Status=" + statusVal + "&StartDate=" + startDate
+				+ "&EndDate=" + endDate + "&ReportFormat=" + format;
+		window.location.href = reporturl;
+	}
+
+	currentReportName = reportName;
+}
+
+function downloadReport1(format) {
+
+	if (currentReportName == "") {
+		alert("Please Select the report.");
+		return;
+	}
+
+	if (currentReportName == "CustomerIssuesDate.rptdesign") {
+		CustomerIssueReport(currentReportName, format);
+	} else if (currentReportName == "CustomersDate.rptdesign") {
+		CustomersReport(currentReportName, format);
+	} else if (currentReportName == "ServiceProviderDate.rptdesign") {
+		ServiceProvidersReport(currentReportName, format);
+	}
 
 }
